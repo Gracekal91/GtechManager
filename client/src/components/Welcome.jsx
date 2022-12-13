1842cad69807da1a48674fe1481f95cacab811ac
Grace Kalombo <grace.kalombo@mondia.com>
2022-12-13 13:01:32 +0200

    GTEC-7-create landing page

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,20 @@
+import '../assets/scss/hero.scss'
+
+const Welcome = ()=>{
+    return(
+        <>
+            <div className="welcome_container">
+                <div className="welcome_content">
+                    <h1>Welcome to G tech</h1>
+                    <p>Once, more you are more then welcome to the house of software
+                    development and cutting-edges technology. Think it and we'll build
+                        as it's in your mind.Once, more you are more then welcome to the house of software
+                        development and cutting-edges technology. Think it and we'll build .
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Welcome
